c0c646e73afcb18b2e64aee771168b17499c9d88
Heeyoun Lim <heeyn.lim@gmail.com>
2024-07-13 14:56:55 +0900

    P1085: escape from rectangle

--- src/P1085.java
+++ src/P1085.java
@@ -1,22 +1,8 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-/**
- * P1085 직사각형 탈출
- *
- * 한수는 지금 (x, y)에 있다. 직사각형은 각 변이 좌표축에 평행하고,
- * 왼쪽 아래 꼭짓점은 (0, 0), 오른쪽 위 꼭짓점은 (w, h)에 있다.
- * 직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
- *
- * 입력
- * 첫째 줄에 x, y, w, h가 주어진다.
- *
- * 출력
- * 첫째 줄에 문제의 정답을 출력한다.
- */
-
-public class Main {
+public class P1085 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -44,5 +30,6 @@ public class Main {
         System.out.println(minDstn);
 
         br.close();
+
     }
 }
